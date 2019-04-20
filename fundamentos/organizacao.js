d8f7b61333aac5a6266a8d5154cc0e01386811cf
Alan Robson <alanrslopes@hotmail.com>
2019-04-20 19:32:22 -0300

    Submetendo o primeiro arquivo.

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,12 @@
+console.log('Sentença de Código'); //Funciona
+console.log(
+  'Outra sentença de código.'
+); // Funciona também
+
+{
+  {
+    // Bloco de Código
+    console.log('Olá ');
+    console.log('mundo!');
+  }
+}
