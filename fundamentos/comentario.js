848d0c0b998aa31f944dbab94f493058464a7581
Alan Robson <alanrslopes@hotmail.com>
2019-04-20 20:21:59 -0300

    Incluindo novos arquivos.

---
+++ fundamentos/comentario.js
@@ -0,0 +1,23 @@
+// Comentário de uma linha
+console.log('Um comentário acima, referente à esta sentença.');
+
+/*
+  Comentários de múltiplas linhas.
+  é utilizada para comentário grandes.
+*/
+console.log('Senença associada ao comentário acima.');
+
+/*
+ * Outra forma de atribuir
+ * comentarios de múltiplas linhas.
+ * sempre utilizando um asterisco no
+ * inicio da linha
+*/
+console.log('Outra sentença associada ao bloco acima.');
+
+// Evitar comentários idiotas...
+// Dê preferência aos códigos legíveis.
+
+// Produza códigos limpos e claros, para evitar ao máximo utilizar comentários. Fazendo com que seu código seja entendido apenas pelos comandos listados, sem a necessidade de comentários.
+
+// Somento utilizar comentários, quando realmente for necessário.
---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,20 @@
+var a = 3;
+let b = 4;
+
+var a = 30;
+b = 40;
+
+console.log(a, b);
+
+a = 300;
+b = 400;
+
+console.log(a, b);
+
+const c = 5;
+// c = 50; Se eu tentar atribuir um novo valor para uma constante, será apresentada uma mensagem de erro.
+
+console.log(c)
+
+
+// Sempre dar preferência para utilizar veriáveis com LET.
